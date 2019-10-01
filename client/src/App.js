ec576cc2a73b021b470afc8fb138aec3789a7497
WillzMu <wilfredmulenga@yahoo.com>
2019-10-01 20:00:18 +0200

    add form to receive user details

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,22 @@ class App extends React.Component {
   handleClick = async () => {
    const data = await fetch(`http://localhost:5000`)
    const res = await data.json()
+   console.log(res)
   }
 
  render(){
+
+  // TODO: configure eslint
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="container">
+      <div className="formContainer">
+      <h1>Rave payment test</h1>
+      <input placeholder="amount" type="number" />
+      <input placeholder="card number" type="number" />
+      <input placeholder="expiry date" type="text" />
+      <input placeholder="cvv" type="number" />
       <button onClick={() => this.handleClick()}>pay me</button>
+      </div>
     </div>
   );
  }
