eb131588616ddd9e74ec7228c01b90fb5fc9e662
WillzMu <wilfredmulenga@yahoo.com>
2019-10-03 21:42:45 +0200

    handle card details input

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,27 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      message: ''
+      amount: '',
+      cardNumber: '',
+      expiryDate: '',
+      cvv: ''
     }
   }
+
   handleClick = async () => {
-   const data = await fetch(`http://localhost:5000`)
+   const { amount, cardNumber, expiryDate, cvv } = this.state
+   const data = await fetch(`http://localhost:5000`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ amount, cardNumber, expiryDate, cvv })
+   })
    const res = await data.json()
    console.log(res)
   }
+
+  handleInput = (field, value) => this.setState({ [field]: value })
 
  render(){
 
@@ -21,10 +34,10 @@ class App extends React.Component {
     <div className="container">
       <div className="formContainer">
       <h1>Rave payment test</h1>
-      <input placeholder="amount" type="number" />
-      <input placeholder="card number" type="number" />
-      <input placeholder="expiry date" type="text" />
-      <input placeholder="cvv" type="number" />
+      <input placeholder="amount" type="number" onChange={(e) => this.handleInput("amount", e.target.value)} />
+      <input placeholder="card number" type="number" onChange={(e) => this.handleInput("cardNumber", e.target.value)} />
+      <input placeholder="expiry date" type="text" onChange={(e) => this.handleInput("expiryDate", e.target.value)} />
+      <input placeholder="cvv" type="number" onChange={(e) => this.handleInput("cvv", e.target.value)} />
       <button onClick={() => this.handleClick()}>pay me</button>
       </div>
     </div>
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,28 @@
 const express = require('express')
 const router = express.Router()
 const crypto = require('crypto')
-const forge = require('node-forge');
-const utf8 = require('utf8');
-const superagent = require('superagent');
+const forge = require('node-forge')
+const superagent = require('superagent')
 
-router.get('/', async (req, res, next) =>  {
+router.post('/', async (req, res, next) => {
+  const { amount, cardNumber, expiryDate, cvv } = req.body
+  const expirymonth = expiryDate.substring(0, 2)
+  const expiryyear = expiryDate.substring(3, 5)
+
   const payload = {
-    "PBFPubKey": process.env.RAVE_PUBLIC_KEY,
-    "cardno": "5438898014560229",
-    "cvv": "890",
-    "expirymonth": "09",
-    "expiryyear": "19",
-    "currency": "NGN",
-    "country": "NG",
-    "amount": "10",
-    "email": "user@gmail.com",
-    "phonenumber": "0902620185",
-    "firstname": "temi",
-    "lastname": "desola",
-    "txRef": "MC-" + Date.now(),// your unique merchant reference
+    'PBFPubKey': process.env.RAVE_PUBLIC_KEY,
+    cardNumber,
+    cvv,
+    expirymonth,
+    expiryyear,
+    'currency': 'NGN',
+    'country': 'NG',
+    amount,
+    'email': 'user@gmail.com',
+    'phonenumber': '0902620185',
+    'firstname': 'temi',
+    'lastname': 'desola',
+    'txRef': 'MC-' + Date.now() // your unique merchant reference
     // "redirect_url": "https://rave-webhook.herokuapp.com/receivepayment"
   }
 
@@ -30,44 +33,43 @@ router.get('/', async (req, res, next) =>  {
     const seckeyadjusted = secKey.replace('FLWSECK-', '')
     const seckeyadjustedfirst12 = seckeyadjusted.substr(0, 12)
     return seckeyadjustedfirst12 + keymd5last12
-}
+  }
 
-const encrypt = (payload) => {
-  payloadJSON = JSON.stringify(payload)
-  let cipher = forge.cipher.createCipher('3DES-ECB', forge.util.createBuffer(getKey()));
-  cipher.start({iv:''});
-  cipher.update(forge.util.createBuffer(payloadJSON, 'utf-8'));
-  cipher.finish();
-  let encrypted = cipher.output;
-  return (forge.util.encode64(encrypted.getBytes()));
-}
+  const encrypt = (payload) => {
+    payloadJSON = JSON.stringify(payload)
+    let cipher = forge.cipher.createCipher('3DES-ECB', forge.util.createBuffer(getKey()))
+    cipher.start({iv: ''})
+    cipher.update(forge.util.createBuffer(payloadJSON, 'utf-8'))
+    cipher.finish()
+    let encrypted = cipher.output
+    return (forge.util.encode64(encrypted.getBytes()))
+  }
 
-// console.log(encrypt(payload))
-
-const raveResponse = (payload) => {
-  superagent
-  .post(process.env.RAVE_CHARGE_ENDPOINT_TEST)
-  .set({ 'Content-Type': 'application/json', 'Accept': 'application/json' })
-  .send({
-    PBFPubKey: process.env.RAVE_PUBLIC_KEY,
-    client: encrypt(payload),
-    alg: '3DES-24'
-  })
-  .end((err, result) => {
-    if(err) {
-      // TODO: handle error
-      res.json({
-        message: err
+  const raveResponse = (payload) => {
+    superagent
+      .post(process.env.RAVE_CHARGE_ENDPOINT_TEST)
+      .set({ 'Content-Type': 'application/json', 'Accept': 'application/json' })
+      .send({
+        PBFPubKey: process.env.RAVE_PUBLIC_KEY,
+        client: encrypt(payload),
+        alg: '3DES-24'
       })
-    }
-    res.json({
-      statusCode: result.statusCode,
-      message: result.ok
-    })
-  })
-}
-// TODO: handle await
-  await raveResponse(payload)
+      .end((err, result) => {
+        if (err) {
+          // TODO: handle error
+          res.json({
+            message: err
+          })
+        }
+        console.log(result.body)
+        res.json({
+          statusCode: result.statusCode,
+          message: result.body
+        })
+      })
+  }
+  // TODO: handle await
+  // await raveResponse(payload)
   // res.json({message: value})
 })
 
