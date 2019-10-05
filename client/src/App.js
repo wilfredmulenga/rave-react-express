d9aa578f8f43b3b1b283579d76a2381784c32a81
WillzMu <wilfredmulenga@yahoo.com>
2019-10-05 20:46:51 +0200

    remove unused users route

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
 
   handleClick = async () => {
    const { amount, cardNumber, expiryDate, cvv } = this.state
-   const data = await fetch(`http://localhost:5000`, {
+   const data = await fetch(`http://localhost:5000/initiate-charge`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -22,10 +22,10 @@ class App extends React.Component {
     body: JSON.stringify({ amount, cardNumber, expiryDate, cvv })
    })
    const res = await data.json()
-   console.log(res.message)
-   if(res.message.message === 'AUTH_SUGGESTION' && res.message.status === 'success') {
-    this.props.history.push('/verify')
-   }
+   console.log(res)
+  //  if(res.message.message === 'AUTH_SUGGESTION' && res.message.status === 'success') {
+  //   this.props.history.push('/verify')
+  //  }
   }
 
   handleInput = (field, value) => this.setState({ [field]: value })
@@ -34,6 +34,8 @@ class App extends React.Component {
 
   // TODO: configure eslint
   // TODO: call this route 'card' and make a separate 'mtn' mobile money route
+  // TODO: clear input fields after pressing the button
+  // TODO: show success, error message, and/or loader after pressing the button
   return (
     <div className="container">
       <div className="formContainer">
