f764af65d68ab9f372295d126e704e7c1cb07c23
WillzMu <wilfredmulenga@yahoo.com>
2019-10-10 23:58:32 +0200

    change response object in charge route

--- routes/initiateCharge.js
+++ routes/initiateCharge.js
@@ -7,6 +7,7 @@ router.post('/', async (req, res, next) => {
   const expirymonth = expiryDate.substring(0, 2)
   const expiryyear = expiryDate.substring(3, 5)
 
+  // use 'Test VisaCard 3D-Secure Authentication' which has an authurl
   try {
     const payload = {
       PBFPubKey: process.env.RAVE_PUBLIC_KEY_TEST,
@@ -30,12 +31,12 @@ router.post('/', async (req, res, next) => {
     const result = await raveResponse(payload)
     res.json({
       statusCode: 200,
-      message: result.data
+      message: result.body.data
     })
   } catch (error) {
     res.json({
       statusCode: 500,
-      message: 'error from provider'
+      message: error
     })
   }
 })
